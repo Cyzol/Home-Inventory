eb674cacba99fda689c51d02fdf740505204d3ea
Cyzol <karol8.9@o2.pl>
2021-04-19 11:03:07 +0200

    box styles and box info view

---
+++ assets/styles/BoxPage.style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerSection: {
+    flex: 0.2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerUserName: {
+    fontStyle: "normal",
+    fontWeight: "normal",
+    fontSize: 24,
+    lineHeight: 42,
+  },
+  boxesGird: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
