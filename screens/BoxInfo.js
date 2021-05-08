87771d3320a36cd21f033b8595687d4d425f24d1
Cyzol <karol8.9@o2.pl>
2021-05-08 14:32:40 +0200

    simple qr code generator

--- screens/BoxInfo.js
+++ screens/BoxInfo.js
@@ -1,46 +1,50 @@
 import React from "react";
-import {View, Text, ScrollView, TouchableOpacity} from "react-native";
-import {styles } from "../assets/styles/BoxInfo.style.js";
-import {auth, db} from "../firebase";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import { styles } from "../assets/styles/BoxInfo.style.js";
+import { auth, db } from "../firebase";
 
-export default function BoxInfo({navigation, route: { params } }) {
+export default function BoxInfo({ navigation, route: { params } }) {
+  const ref = db
+    .collection("users")
+    .doc(auth.currentUser.uid)
+    .collection("boxes");
 
-    const ref = db
-        .collection("users")
-        .doc(auth.currentUser.uid)
-        .collection("boxes");
-
-   const handleDeleteBox = () => {
-        ref
-            .doc(params.id)
-            .delete()
-            .then(()=> navigation.goBack())
-            .catch((error) => {console.error("Error removing document: ", error)});
-   }
-
+  const handleDeleteBox = () => {
+    ref
+      .doc(params.id)
+      .delete()
+      .then(() => navigation.goBack())
+      .catch((error) => {
+        console.error("Error removing document: ", error);
+      });
+  };
 
   return (
-      <View style={styles.container}>
-          <TouchableOpacity
-              style={styles.loginButton}
-              onPress={handleDeleteBox}
-          ><Text>Delete box</Text>
-          </TouchableOpacity>
-          <View  style={styles.containerTitle}>
-              <Text style={styles.titles}>Place: </Text>
-              <View style={styles.place}><Text style={styles.text}>{params.place}</Text></View>
-          </View>
-          <View  style={styles.containerTitle}>
-              <Text style={styles.titles}>Description: </Text>
-              <View style={styles.place}><Text style={styles.text}>{params.description}</Text></View>
-          </View>
-
-          <View contentContainerStyle={styles.scrollView}>
-              <Text style={styles.titles}>Items: </Text>
-              {params.listItems.map((item, index) => (
-                  <View style={styles.item}><Text style={styles.text}>{`Item ${index}: ${item}`}</Text></View>
-              ))}
-          </View>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.loginButton} onPress={handleDeleteBox}>
+        <Text>Delete box</Text>
+      </TouchableOpacity>
+      <View style={styles.containerTitle}>
+        <Text style={styles.titles}>Place: </Text>
+        <View style={styles.place}>
+          <Text style={styles.text}>{params.place}</Text>
+        </View>
       </View>
+      <View style={styles.containerTitle}>
+        <Text style={styles.titles}>Description: </Text>
+        <View style={styles.place}>
+          <Text style={styles.text}>{params.description}</Text>
+        </View>
+      </View>
+
+      <View contentContainerStyle={styles.scrollView}>
+        <Text style={styles.titles}>Items: </Text>
+        {params.listItems.map((item, index) => (
+          <View style={styles.item} >
+            <Text style={styles.text} >{item}</Text>
+          </View>
+        ))}
+      </View>
+    </View>
   );
 }
--- screens/RegisterPage.js
+++ screens/RegisterPage.js
@@ -1,19 +1,16 @@
 import React, { useState } from "react";
 import {
-  StyleSheet,
   Text,
   View,
-  SafeAreaView,
   Image,
   TextInput,
   TouchableOpacity,
-  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
 } from "react-native";
 
 import { styles } from "../assets/styles/LoginPage.style.js";
 import { auth } from "../firebase.js";
-import LoginPage from "./LoginPage.js";
-
 
 export default function RegisterPage() {
   const [name, setName] = useState("");
@@ -22,51 +19,52 @@ export default function RegisterPage() {
 
   const register = () => {
     auth
-    .createUserWithEmailAndPassword(login, password)
-    .then((authUser) => {
-      authUser.user.updateProfile({
-        displayName: name,
-      });
-    })
-    .catch((error) => alert(error.message));
+      .createUserWithEmailAndPassword(login, password)
+      .then((authUser) => {
+        authUser.user.updateProfile({
+          displayName: name,
+        });
+      })
+      .catch((error) => alert(error.message));
   };
 
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.loginIcon}
-        source={require("../assets/icons/registerPage.png")}
-      />
-      <View style={styles.inputContainer}>
-        <Text style={styles.titles}>Name</Text>
-        <TextInput
-          style={styles.loginInput}
-          value={name}
-          onChangeText={(name) => setName(name)}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Image
+          style={styles.loginIcon}
+          source={require("../assets/icons/registerPage.png")}
         />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.titles}>E-mail</Text>
-        <TextInput
-          style={styles.loginInput}
-          value={login}
-          onChangeText={(login) => setLogin(login)}
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.titles}>Password</Text>
-        <TextInput
-          secureTextEntry={true}
-          style={styles.loginInput}
-          value={password}
-          onChangeText={(password) => setPassword(password)}
-        />
-      </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.titles}>Name</Text>
+          <TextInput
+            style={styles.loginInput}
+            value={name}
+            onChangeText={(name) => setName(name)}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.titles}>E-mail</Text>
+          <TextInput
+            style={styles.loginInput}
+            value={login}
+            onChangeText={(login) => setLogin(login)}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.titles}>Password</Text>
+          <TextInput
+            secureTextEntry={true}
+            style={styles.loginInput}
+            value={password}
+            onChangeText={(password) => setPassword(password)}
+          />
+        </View>
 
-      <TouchableOpacity style={styles.loginButton} onPress={register}>
-        <Text style={styles.loginText}>Register</Text>
-      </TouchableOpacity>
-
-    </View>
+        <TouchableOpacity style={styles.loginButton} onPress={register}>
+          <Text style={styles.loginText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
